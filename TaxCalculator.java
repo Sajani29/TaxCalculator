2a81a2cfdf4e0eddbb611e36e22641198fbc5c7f
Sajani Siriwardhana <22cis0309@ms.sab.ac.lk>
2026-04-18 03:40:27 +0530

    Add files via upload

---
+++ TaxCalculator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class TaxCalculator{
+	public static void main(String[] args){
+		System.out.println("\t\t\t\t\t __  ________ ________ ________");
+		System.out.println("\t\t\t\t\t|  \\/        \\|       |        \\");
+		System.out.println("\t\t\t\t\t \\$|   $$$$$$ | $$$$$$$$\\$$$$$$$$");
+		System.out.println("\t\t\t\t\t|  | $$     \\$| $$__      | $$");
+		System.out.println("\t\t\t\t\t| $| $$       | $$  \\     | $$");
+		System.out.println("\t\t\t\t\t| $| $$    ___| $$$$$     | $$");
+		System.out.println("\t\t\t\t\t| $| $$___/   | $$_____   | $$");
+		System.out.println("\t\t\t\t\t| $$\\$$      $| $$     \\  | $$");
+		System.out.println("\t\t\t\t\t \\$$ \\$$$$$$   \\$$$$$$$$   \\$$");
+	}
+}
